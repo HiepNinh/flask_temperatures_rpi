e799defcd24d2c815f433b4d69a75b88c0e73ffc
NinhKim <kimhiepninh02121997@gmail.com>
2021-02-07 15:44:52 +0700

    Adding layout

--- blueprints/lab_temp/routes.py
+++ blueprints/lab_temp/routes.py
@@ -8,10 +8,13 @@ lab_temp = Blueprint('lab_temp', __name__)
 
 @lab_temp.route("/lab_temp")
 def show_lab_temp():
-   dhtDevice = adafruit_dht.DHT22(board.D17, use_pulseio = False)
-   temperature = dhtDevice.temperature
-   humidity = dhtDevice.humidity
-   if humidity is not None and temperature is not None:
-      return render_template("lab_temps/lab_temp.html",temp=temperature,hum=humidity)
-   else:
-         return render_template("errors/no_sensor.html")
+   try:
+      dhtDevice = adafruit_dht.DHT22(board.D17, use_pulseio = False)
+      temperature = dhtDevice.temperature
+      humidity = dhtDevice.humidity
+      if humidity is not None and temperature is not None:
+         return render_template("lab_temps/lab_temp.html",temp=temperature,hum=humidity, title="Lab Conditions by RPi")
+      else:
+            return render_template("errors/no_sensor.html")
+   except:
+      return render_template("errors/no_sensor.html")
