c3fd1c144ef81fa999e797e8b5f49d373ef01ac3
NinhKim <kimhiepninh02121997@gmail.com>
2021-02-07 12:23:24 +0700

    Test 2

--- blueprints/lab_temp/routes.py
+++ blueprints/lab_temp/routes.py
@@ -8,13 +8,10 @@ lab_temp = Blueprint('lab_temp', __name__)
 
 @lab_temp.route("/lab_temp")
 def show_lab_temp():
-   try:
-      dhtDevice = adafruit_dht.DHT22(board.D17)
-      temperature = dhtDevice.temperature
-      humidity = dhtDevice.humidity
-      if humidity is not None and temperature is not None:
-         return render_template("lab_temps/lab_temp.html",temp=temperature,hum=humidity)
-      else:
-       	 return render_template("errors/no_sensor.html")
-   except:
-      return render_template("errors/no_sensor.html")
+   dhtDevice = adafruit_dht.DHT22(board.D17)
+   temperature = dhtDevice.temperature
+   humidity = dhtDevice.humidity
+   if humidity is not None and temperature is not None:
+      return render_template("lab_temps/lab_temp.html",temp=temperature,hum=humidity)
+   else:
+         return render_template("errors/no_sensor.html")
